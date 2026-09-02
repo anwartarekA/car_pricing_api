07024a847a01cdf165a28b8a5e9befe937af6c22
= <=>
2026-09-02 21:38:57 +0100

    compelete the signup function

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,8 @@
 import { UserService } from './user.service';
 import { Injectable, NotFoundException } from '@nestjs/common';
-
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+const scrypt = promisify(_scrypt);
 @Injectable()
 export class AuthService {
   constructor(private userService: UserService) {}
@@ -9,5 +11,15 @@ export class AuthService {
     // check if email is in use
     const users = await this.userService.find(email);
     if (users.length) throw new NotFoundException('Email already in use');
+    // encrypt the password
+    // 1) create the salt
+    const salt = randomBytes(8).toString('hex');
+    // 2) hash the password and salt together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    // 3) join the hash and salt together
+    const result = salt + '.' + hash.toString('hex');
+    // create a new user and save it
+    const user = await this.userService.create(email, result);
+    return user;
   }
 }
