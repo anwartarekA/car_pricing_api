74564db80b8956e95f3c7b07644d43bde103636e
= <=>
2026-09-02 21:44:52 +0100

    signup user

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -14,13 +14,17 @@ import { UserService } from './user.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
 import { serialize } from '../interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
 @Controller('auth')
 @serialize(UserDto)
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    return this.userService.create(body.email, body.password);
+    return this.authService.signup(body.email, body.password);
   }
   @Get()
   findAll(@Query('email') query: string) {
