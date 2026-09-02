fc9cecaa5c1581fb8605ccc5b725beea91d94c8d
= <=>
2026-09-02 20:32:14 +0100

    create sign up

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+  // signup
+  async signup(email: string, password: string) {
+    // check if email is in use
+    const users = await this.userService.find(email);
+    if (users.length) throw new NotFoundException('Email already in use');
+  }
+}
