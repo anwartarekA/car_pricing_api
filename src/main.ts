dd19966e424b55b9446c153e12374966d329c411
= <=>
2026-09-03 02:37:28 +0100

    use session cookie library for passing user id into session

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { sessionCookie } from 'session-cookie';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
+    }),
+    sessionCookie({
+      keys: ['anwartarek123@'],
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
